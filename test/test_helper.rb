5afb8a9172af0ae6800e49f869559cae873fba81
Daniel Cadenas <dcadenas@gmail.com>
2009-09-30 22:53:07 -0300

    Initial commit to stub_helper.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'stub_helper'
+
+class Test::Unit::TestCase
+end
